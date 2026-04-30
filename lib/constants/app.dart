b07d47780e356d0f0138d86a9149be14b0eeaf74
Graham Williams <graham.williams@togaware.com>
2026-05-01 09:20:04 +1000

    Bump version 0.1.13 Updated code. Update to solidui server list

--- lib/constants/app.dart
+++ lib/constants/app.dart
@@ -1,14 +1,37 @@
-/// BillPod app-wide constants.
+/// BillPod - app-wide constants.
 ///
-// Time-stamp: <2026-04-14>
+// Time-stamp: <Wednesday 2026-04-29 15:15:37 +1000 Graham Williams>
 ///
 /// Copyright (C) 2026, Togaware Pty Ltd
 ///
-/// Licensed under the GNU General Public License, Version 3
+/// Licensed under the GNU General Public License, Version 3 (the "License");
+///
+/// License: https://opensource.org/license/gpl-3-0
+//
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+// details.
+//
+// You should have received a copy of the GNU General Public License along with
+// this program.  If not, see <https://opensource.org/license/gpl-3-0>.
+///
+/// Authors: Graham Williams
 
 library;
 
 const String appName = 'BilliPod';
+
+/// Application title displayed as the window title.
+
+const String appTitle = 'BilliPod - Manage Your Bills';
+
 const String appDirectory = 'billipod';
 const String billsFileName = 'bills.ttl';
 
